43fe18e0e1f298fcae87dd9d99c73756a2f0d2d3
ChrisBenua <kpbenua@edu.hse.ru>
2020-01-16 20:37:13 +0300

    cors fix

--- backend/server/src/main/java/com/groudina/ten/demo/controllers/CompetitionsController.java
+++ backend/server/src/main/java/com/groudina/ten/demo/controllers/CompetitionsController.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,7 @@ import java.util.List;
 
 @Log4j2
 @RequestMapping(path="/api/competitions", produces = {MediaType.APPLICATION_JSON_VALUE})
+@CrossOrigin(origins = {"*"}, maxAge = 3600)
 @Controller
 public class CompetitionsController {
     private DbCompetitionsRepository competitionsRepository;
