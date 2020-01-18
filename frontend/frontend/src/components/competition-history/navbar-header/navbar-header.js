2ff87537bdea8adb49b62bd333b29ec4b6fcdf5c
ChrisBenua <kpbenua@edu.hse.ru>
2020-01-18 16:57:45 +0300

    added routing

--- frontend/frontend/src/components/competition-history/navbar-header/navbar-header.js
+++ frontend/frontend/src/components/competition-history/navbar-header/navbar-header.js
@@ -1,8 +1,18 @@
 import React from "react";
 import DefaultSubmitButton from "../../common/default-submit-button";
+import {withRouter} from "react-router-dom";
 import './navbar-header.css';
 
 class NavbarHeader extends React.Component {
+
+    onCreateGameClick = () => {
+        this.props.history.push('/competitions/create');
+    };
+
+    onGameHistoryClick = () => {
+        this.props.history.push('/competitions/history');
+    };
+
     render() {
         const buttonsStyle = {
             marginBottom: "0.7rem",
@@ -15,12 +25,12 @@ class NavbarHeader extends React.Component {
         return (
             <div className="navbar-header-fixed-top">
                 <div className={"d-flex"} style={{marginTop: "20px", marginLeft: "40px"}}>
-                    <DefaultSubmitButton text="История игр" style={{...buttonsStyle, marginRight: "50px"}}/>
-                    <DefaultSubmitButton text="Создать игру" style={buttonsStyle}/>
+                    <DefaultSubmitButton text="История игр" style={{...buttonsStyle, marginRight: "50px"}} onClick={this.onGameHistoryClick}/>
+                    <DefaultSubmitButton text="Создать игру" style={buttonsStyle} onClick={this.onCreateGameClick}/>
                 </div>
             </div>
         )
     }
 }
 
-export default NavbarHeader;
+export default withRouter(NavbarHeader);
