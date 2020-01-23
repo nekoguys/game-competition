cfda3d991a47301792556b207f13e0ddc7aa5d16
Сергей Фомин <sdfomin@edu.hse.ru>
2020-01-23 18:31:39 +0300

    created tests for pin

--- backend/server/src/test/java/com/groudina/ten/demo/controllers/CompetitionsControllerTest.java
+++ backend/server/src/test/java/com/groudina/ten/demo/controllers/CompetitionsControllerTest.java
@@ -124,4 +124,63 @@ class CompetitionsControllerTest {
         assertEquals(1, competitionsRepository.findAll().count().block());
         assertEquals(competitionsRepository.findAll().collect(Collectors.toList()).block().get(0).getOwner(), owner);
     }
+
+    @Test
+    @WithMockUser(value = "email", password = "1234", roles = {"TEACHER"})
+    void checkPinCompetition() {
+        var owner = userRepository.save(DbUser.builder().password("1234").email("email").roles(rolesRepository.findAll().collect(Collectors.toList()).block()).build()).block();
+        assertEquals(0, competitionsRepository.findAll().count().block());
+
+        webTestClient.post().uri("/api/competitions/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(NewCompetition.builder()
+                        .shouldShowStudentPreviousRoundResults(false)
+                        .state("draft")
+                        .maxTeamsAmount(1)
+                        .demandFormula(List.of("1", "2"))
+                        .expensesFormula(List.of("1", "2", "3"))
+                        .instruction("instr")
+                        .name("name")
+                        .roundLength(1)
+                        .roundsCount(2)
+                        .shouldEndRoundBeforeAllAnswered(false)
+                        .shouldShowResultTableInEnd(false)
+                        .maxTeamSize(3)
+                        .build()))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange().expectStatus().isOk()
+                .expectBody(ResponseMessage.class)
+        ;
+        assertEquals(competitionsRepository.findAll().collect(Collectors.toList()).block().get(0).getPin(), null);
+
+        webTestClient.post().uri("/api/competitions/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(NewCompetition.builder()
+                        .shouldShowStudentPreviousRoundResults(false)
+                        .state("registration")
+                        .maxTeamsAmount(1)
+                        .demandFormula(List.of("1", "2"))
+                        .expensesFormula(List.of("1", "2", "3"))
+                        .instruction("instr")
+                        .name("name")
+                        .roundLength(1)
+                        .roundsCount(2)
+                        .shouldEndRoundBeforeAllAnswered(false)
+                        .shouldShowResultTableInEnd(false)
+                        .maxTeamSize(3)
+                        .build()))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange().expectStatus().isOk()
+                .expectBody(ResponseMessage.class)
+        ;
+
+        var t = competitionsRepository
+                .findAll()
+                .toStream()
+                .filter(x -> x.getPin() != null)
+                .collect(Collectors.toList())
+                .get(0);
+        assertNotEquals(t.getPin(), null);
+        assertEquals(competitionsRepository.findByPin(t.getPin()).block().getPin(), t.getPin());
+    }
 }
