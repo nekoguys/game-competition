31795feab1895f5e2891dc46605e3bef3b613c7a
ChrisBenua <kpbenua@edu.hse.ru>
2020-06-06 12:48:25 +0300

    team name validation on frontend

--- frontend/frontend/src/components/join-competition/join-competition-captain-form/join-competition-form.js
+++ frontend/frontend/src/components/join-competition/join-competition-captain-form/join-competition-form.js
@@ -9,11 +9,20 @@ class JoinCompetitionForm extends React.Component {
     constructor(props) {
         super(props);
 
-        this.formState = {};
+        this.formState = {
+            teamName: "",
+            password: ""
+        };
+
+        this.state = {
+            isEnterGameButtonEnabled: false
+        }
     }
 
     updateFormStateField(key, value) {
         this.formState[key] = value;
+
+        this.setState({isEnterGameButtonEnabled: this.formState['teamName'].length >= 4 && this.formState['password'].length >= 4});
     }
 
     render() {
@@ -69,7 +78,7 @@ class JoinCompetitionForm extends React.Component {
                             paddingTop: "10px",
                             paddingBottom: "10px",
                             flexGrow: "0.2",
-                        }} onClick={() => onFormSubmit(this.formState)}/>
+                        }} isDisabled={!this.state.isEnterGameButtonEnabled} onClick={() => onFormSubmit(this.formState)}/>
                     </div>
                 </div>
             </div>
