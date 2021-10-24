2b0c40fa910118a254ad8b4c2c9beafb231dd84b
Kristian Benua <chris-benua@yandex-team.ru>
2021-10-24 14:07:33 +0300

    Updated web security. Permitted all requests

---
+++ backend/app/web/src/main/java/ru/selemilka/game/web/configs/WebSecurityConfig.kt
@@ -0,0 +1,28 @@
+package ru.selemilka.game.web.configs
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity.http
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@Configuration
+@EnableReactiveMethodSecurity
+@EnableWebFluxSecurity
+class WebSecurityConfig {
+    @Bean
+    fun springWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http.cors().disable()
+                .csrf().disable()
+                .authorizeExchange().anyExchange().permitAll()
+                .and().build()
+    }
+
+    @Bean
+    fun passwordEncoder(): BCryptPasswordEncoder? {
+        return BCryptPasswordEncoder()
+    }
+}
