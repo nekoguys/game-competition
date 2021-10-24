61c9a59808acc9580397f1d4ccf87523304e3b09
pema4 <petelin.maksim@live.com>
2021-10-24 14:21:40 +0300

    INFRA-11: end files with new line (why it is not automated in IDEA...)

--- backend/app/web/src/main/java/ru/selemilka/game/web/configs/WebSecurityConfig.kt
+++ backend/app/web/src/main/java/ru/selemilka/game/web/configs/WebSecurityConfig.kt
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.config.web.server.ServerHttpSecurity.http
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.server.SecurityWebFilterChain
 
