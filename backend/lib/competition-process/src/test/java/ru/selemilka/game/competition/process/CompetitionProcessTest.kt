8a1639e85056c9335513cae62617a4538c29e0ab
Kristian Benua <chris-benua@yandex-team.ru>
2021-10-24 15:43:40 +0300

    Sample tests + github actions

---
+++ backend/lib/competition-process/src/test/java/ru/selemilka/game/competition/process/CompetitionProcessTest.kt
@@ -0,0 +1,10 @@
+package ru.selemilka.game.competition.process
+
+import org.junit.jupiter.api.Test
+
+internal class CompetitionProcessTest {
+
+    @Test
+    fun testCompetitionProcess() {
+    }
+}
---
+++ backend/lib/user/src/test/java/ru/selemilka/game/user/storage/UserStorageTest.kt
@@ -0,0 +1,8 @@
+package ru.selemilka.game.user.storage
+
+import org.junit.jupiter.api.Test
+
+internal class UserStorageTest {
+    @Test
+    fun testUserStorage() {}
+}
