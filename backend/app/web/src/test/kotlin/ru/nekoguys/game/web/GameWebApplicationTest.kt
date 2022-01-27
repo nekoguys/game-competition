dd42661aa4955ed682086ac67d7384eca1746f55
pema4 <petelin.maksim@live.com>
2022-01-27 22:29:15 +0300

    BACKEND-36: make sure everything is kotlin now
    Rename .java to .kt

---
+++ backend/app/web/src/test/kotlin/ru/nekoguys/game/web/GameWebApplicationTest.kt
@@ -0,0 +1,8 @@
+package ru.nekoguys.game.web.ru.nekoguys.game.web
+
+import org.junit.jupiter.api.TestInstance
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+annotation class GameWebApplicationTest
--- backend/lib/game/src/test/java/ru/nekoguys/game/competition/CompetitionTest.kt
+++
@@ -1,9 +0,0 @@
-package ru.nekoguys.game.competition
-
-import org.junit.jupiter.api.Test
-
-internal class CompetitionTest {
-    @Test
-    fun testCompetition() {
-    }
-}
