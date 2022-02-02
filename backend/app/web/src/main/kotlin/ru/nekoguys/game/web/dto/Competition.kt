c949e6b4865acf652ffe8cfc1f2d0a634001cbae
pema4 <petelin.maksim@live.com>
2022-02-03 00:36:57 +0300

    BACKEND-47: fix CreateCompetitionRequest deserialization

--- backend/app/web/src/main/kotlin/ru/nekoguys/game/web/dto/Competition.kt
+++ backend/app/web/src/main/kotlin/ru/nekoguys/game/web/dto/Competition.kt
@@ -3,7 +3,7 @@ package ru.nekoguys.game.web.dto
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
-import com.fasterxml.jackson.databind.ser.impl.IndexedStringListSerializer
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
 import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
@@ -11,12 +11,12 @@ import javax.validation.constraints.Size
 data class CreateCompetitionRequest(
     @NotNull
     @Size(min = 2, max = 2, message = "Demand formula should contain 2 values separated by ;")
-    @JsonSerialize(using = IndexedStringListSerializer::class)
+    @JsonSerialize(contentUsing = ToStringSerializer::class)
     val demandFormula: List<Double>,
 
     @NotNull
     @Size(min = 3, max = 3, message = "Expenses formula should contain 3 values separated by ;")
-    @JsonSerialize(using = IndexedStringListSerializer::class)
+    @JsonSerialize(contentUsing = ToStringSerializer::class)
     val expensesFormula: List<Double>,
 
     val instruction: String? = null,
