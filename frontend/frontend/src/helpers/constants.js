bfa355cb48a7582196289d5057059912e6bc9d1a
Christian Benua <kpbenua@edu.hse.ru>
2022-03-25 01:08:57 +0300

    price stream fix

--- frontend/frontend/src/helpers/constants.js
+++ frontend/frontend/src/helpers/constants.js
@@ -1,5 +1,5 @@
 export const MESSAGE_EVENT_ID = "messagesStream";
-export const PRICE_EVENT_ID = "priceStream";
+export const PRICE_EVENT_ID = "pricesStream";
 export const ANSWER_EVENT_ID = "answerStream";
 export const RESULT_EVENT_ID = "resultStream";
 export const ROUND_EVENT_ID = "roundStream";
